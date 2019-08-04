0239f67cabe4956df19a84cc94345ff5a59d7337
xiaohanjc00 <xiaohanjc00@gmail.com>
2019-08-04 11:51:49 +0200

    Add reservation table

--- src/menu.java
+++ src/menu.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -14,6 +15,7 @@ public class menu extends Application {
     public void start(Stage primaryStage) {
         Pane root = new VBox();
         menuBar(root);
+        tableView1(root);
 
         Scene scene = new Scene(root);
         primaryStage.setTitle("Restaurant Management");
@@ -31,5 +33,25 @@ public class menu extends Application {
         Menu aboutMenu = new Menu("About");
 
         menubar.getMenus().addAll(toolsMenu,helpMenu,aboutMenu);
+    }
+
+    public void tableView1(Pane parent)
+    {
+        TableView<Client> table1 = new TableView();
+        parent.getChildren().add(table1);
+
+        TableColumn<Client,String> column1 = new TableColumn<>("name");
+        column1.setCellValueFactory(new PropertyValueFactory<Client, String>("name"));
+
+        TableColumn<Client, Integer> column2 = new TableColumn<>("Table Number");
+        column2.setCellValueFactory(new PropertyValueFactory<Client, Integer>("tableNumber"));
+
+        TableColumn<Client, Integer> column3 = new TableColumn<>("Number of Person");
+        column3.setCellValueFactory(new PropertyValueFactory<Client, Integer>("nnumberOfPerson"));
+
+        TableColumn<Client, Integer> column4 = new TableColumn<>("Phone Number");
+        column4.setCellValueFactory(new PropertyValueFactory<Client, Integer>("phoneNumber"));
+
+        table1.getColumns().addAll(column1, column2, column3, column4);
     }
 }
